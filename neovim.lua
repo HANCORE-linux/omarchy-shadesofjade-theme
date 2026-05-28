d13c56d92fc688cb9c8e2bacf6aa1b570fb49757
HANCORE <m.fattah-ahmad@gmx.de>
2026-05-28 15:31:52 +0200

    Add files via upload

--- neovim.lua
+++ neovim.lua
@@ -7,33 +7,32 @@ return {
     opts = {
       transparent = false,
       colors = {
-        -- Backgrounds & UI
+
         bg           = "#00110b",
         bg_dark      = "#00110b",
-        bg_highlight = "#3d584d", -- Using color15 for subtle highlight contrast
+        bg_highlight = "#3d584d", 
 
         -- Foregrounds
         fg           = "#e6f6f0",
-        fg_dark      = "#80d4b5", -- Using color6 for a slightly darker/dimmed foreground
-        comment      = "#87ae83", -- Using color8 for a readable, distinct comment green
+        fg_dark      = "#49695b", 
+        comment      = "#006540", 
 
         -- Terminal/Syntax Palette
-        red          = "#ff3370", -- color1 / color9
-        orange       = "#E25F4B", -- cursor / selection_background
-        yellow       = "#cceee1", -- color3
-        green        = "#00a86b", -- color2 / color10
-        cyan         = "#80d4b5", -- color6 / color14
-        blue         = "#006540", -- color12
-        purple       = "#D1FFb0", -- accent / color4
-        magenta      = "#8fc85c", -- color5 / color13
+        red          = "#ff3370", 
+        orange       = "#E25F4B", 
+        yellow       = "#cceee1", 
+        green        = "#00a86b", 
+        cyan         = "#80d4b5", 
+        blue         = "#87ae83", 
+        purple       = "#D1FFb0", 
+        magenta      = "#8fc85c", 
       },
 	  on_highlights = function(hl, c)
-    -- Your existing lines
     hl.CursorLine = { bg = "#0c1d17" } 
     hl.CursorLineNr = { fg = c.orange, bold = true }
     hl["@markup.raw.markdown_inline"] = { bg = "NONE" }
     hl["@markup.raw.block.markdown"] = { bg = "NONE" }
-	hl["@markup.quote"] = { bg = "NONE" }
+  	hl["@markup.quote"] = { bg = "NONE" }
    end,
     },
     config = function(_, opts)
